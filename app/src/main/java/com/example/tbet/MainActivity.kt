67a80ac733527e57e9d832689132899098711279
carlos fuentes <eduardofuentesingmeca@gmail.com>
2023-03-16 16:13:00 -0400

    feat: splash

--- app/src/main/java/com/example/tbet/MainActivity.kt
+++ app/src/main/java/com/example/tbet/MainActivity.kt
@@ -2,24 +2,25 @@ package com.example.tbet
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.webkit.*
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
 
 
-        val visor = findViewById<WebView>(R.id.web);
-        visor.webChromeClient = object : WebChromeClient(){};
-        visor.webViewClient = object : WebViewClient(){};
+        val webView = findViewById<WebView>(R.id.web);
+        webView.webChromeClient = object : WebChromeClient(){};
+        webView.webViewClient = object : WebViewClient(){};
 
-        val webSettings: WebSettings = visor.getSettings();
+        val webSettings: WebSettings = webView.getSettings();
         webSettings.javaScriptEnabled = true;
         webSettings.javaScriptCanOpenWindowsAutomatically = true;
         webSettings.domStorageEnabled = true;
@@ -30,8 +31,19 @@ class MainActivity : AppCompatActivity() {
         cookieManager.setAcceptCookie(true)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            visor.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
-        visor.loadUrl("https://tmichezo.co.tz/");
+
+        val splashScreen = findViewById<LinearLayout>(R.id.splash_screen_layout);
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                splashScreen.visibility = View.GONE
+                webView.visibility = View.VISIBLE
+            }
+        }
+
+        webView.loadUrl("https://tmichezo.co.tz/");
     }
 }
